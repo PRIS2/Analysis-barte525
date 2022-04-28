7fd68153f7a7b0cacd492748d3011bcd5a036b1e
barte525 <72998281+barte525@users.noreply.github.com>
2022-04-28 12:58:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,28 +13,6 @@ def run_ten(sim):
 
 
 if __name__ == '__main__':
-    # loops = 1000
-    # roulette_power = 5
-    # tournament_size = 2
-    # amount_to_cross = 3
-    # mutate_probability = 0.01
-    # easy_test = Test('easy')
-    # flat_test = Test('flat')
-    # hard_test = Test('hard')
-    # easy_test.roulette_test(loops, roulette_power)
-    # flat_test.roulette_test(loops, roulette_power)
-    # hard_test.roulette_test(loops, roulette_power)
-    # easy_test.tournament_test(loops, tournament_size)
-    # flat_test.tournament_test(loops, tournament_size)
-    # hard_test.tournament_test(loops, tournament_size)
-    # easy_test.crossover_test(amount_to_cross)
-    # flat_test.crossover_test(amount_to_cross)
-    # m1, m2 = hard_test.crossover_test(amount_to_cross)
-    # print(hard_test.check_hard_matrix(m1))
-    # print(hard_test.check_hard_matrix(m2))
-    # easy_test.mutate_test(mutate_probability)
-    # flat_test.mutate_test(mutate_probability)
-    # hard_test.mutate_test(mutate_probability)
     population_size = 500
     number_of_generations = 50
     is_tournament = True
@@ -47,9 +25,6 @@ if __name__ == '__main__':
                      crossover_probability, mutation_probability, case)
     sim.run_simulation(plot=True)
     run_ten(sim)
-    #data = Data('data/hard_cost.json', 'data/hard_flow.json', 24).cost_flow_array
-
-    #sim.run_random()
 
 
 
